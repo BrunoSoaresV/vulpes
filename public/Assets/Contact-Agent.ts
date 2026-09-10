16c084d33deecbdd36cae30517e44e840696f8da
Bruno Soares Veríssimo <94240946+BrunoSoaresV@users.noreply.github.com>
2026-09-10 12:07:57 -0300

    Refactor contact links in Contact-Agent.ts
    Updated WhatsApp contact links for better formatting and consistency.

--- public/Assets/Contact-Agent.ts
+++ public/Assets/Contact-Agent.ts
@@ -1,6 +1,17 @@
-export const contactAgent = 'https://wa.me/5595984250254';
-export const jouneyContact = 'https://api.whatsapp.com/send/?phone=5595984250254&text=Ol%C3%A1%21+Gostaria+de+saber+mais+sobre+as+aulas+ingl%C3%AAs+da+Vulpes&type=phone_number&app_absent=0';
-export const overseasContact = 'https://api.whatsapp.com/send/?phone=5595984250254&text=Ol%C3%A1%21+Gostaria+de+saber+mais+informa%C3%A7%C3%B5es+sobre+o+Overseas&type=phone_number&app_absent=0';
-export const proAceContact = 'https://api.whatsapp.com/send/?phone=5595984250254&text=Ol%C3%A1%21+Gostaria+de+saber+mais+informa%C3%A7%C3%B5es+sobre+o+Pro+ace&type=phone_number&app_absent=0';
-export const proBusinessMasterClass = 'https://chk.eduzz.com/1897833';
-export const proConsultoriaVlps = 'https://api.whatsapp.com/send/?phone=5595984250254&text&type=phone_number&app_absent=0';
+export const contactAgent =
+  'https://wa.me/5595984250254';
+
+export const jouneyContact =
+  'https://wa.me/5595984250254?text=Ol%C3%A1%21%20Gostaria%20de%20saber%20mais%20sobre%20as%20aulas%20de%20ingl%C3%AAs%20da%20Vulpes';
+
+export const overseasContact =
+  'https://wa.me/5595984250254?text=Ol%C3%A1%21%20Gostaria%20de%20saber%20mais%20informa%C3%A7%C3%B5es%20sobre%20o%20Overseas';
+
+export const proAceContact =
+  'https://wa.me/5595984250254?text=Ol%C3%A1%21%20Gostaria%20de%20saber%20mais%20informa%C3%A7%C3%B5es%20sobre%20o%20Pro%20Ace';
+
+export const proBusinessMasterClass =
+  'https://chk.eduzz.com/1897833';
+
+export const proConsultoriaVlps =
+  'https://wa.me/5595984250254';
